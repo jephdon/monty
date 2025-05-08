c35944b2c8d5d662c45611532ed872be4661a6f1
jephdon <chukwujekwujephthah@gmail.com>
2025-05-08 16:35:06 -0700

    Implement the nop opcode

---
+++ bytecodes/13.m
@@ -0,0 +1,5 @@
+push 1
+nop
+push 2
+nop
+pall
---
+++ op_nop.c
@@ -0,0 +1,14 @@
+#include "monty.h"
+
+/**
+ * op_nop - Does virtually nothing, Used as a placeholder
+ * @stack: Double pointer to the head of the stack
+ * @line_number: line number
+ *
+ * Return: Nothing
+ */
+void op_nop(stack_t **stack, unsigned int line_number)
+{
+	(void)stack;
+	(void)line_number;
+}
